fc58d4089b9f694c1d6af110aa3cfed8ce74c498
OchiengJr <chrispine.ochieng@student.moringaschool.com>
2024-03-22 22:44:11 +0300

    React Router Code-Along

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Separate Home component into its own file for better organization
+
 function Home() {
   return (
     <div>
@@ -9,8 +11,12 @@ function Home() {
   );
 }
 
+// Main App component
+
 function App() {
   return <Home />;
 }
+
+// Render the App component to the root element
 
 ReactDOM.render(<App />, document.getElementById("root"));
